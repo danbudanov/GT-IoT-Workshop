7adf480839e6dd0a0a49ddf4fb0546978a98a9b5
Daniil Budanov <danbudanov@gmail.com>
2016-11-10 00:04:04 -0500

    server workz

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 from flask import Flask, request, render_template, redirect
-import json
+import json, datetime
+# from apscheduler import Scheduler
 app = Flask(__name__)
 
+# def get_time(time_str):
 
 alarm_info = {
-        'time' : '12:30',
-        'date': '9/29/13',
-        'set': True,
-        'message': "hi!"
+        'set': False,
         }
+
+# sched = Scheduler()
+# s
 @app.route('/', methods=['GET', 'POST'])
 def handle_alarm():
+    global alarm_time;
     if request.method == 'POST':
-        # parse the json
-        print "POSTed!"
-        print request.form
+        for i in request.form:
+            alarm_info[i] = request.form[i]
+        print "Alarm was set to {} on {}".format(alarm_info['time'], alarm_info['date'])
+        alarm_info["set"] = True
+        alarm_time = datetime.datetime.strptime((alarm_info["time"] + " " \
+            + alarm_info["date"]), "%H:%M %Y-%m-%d")
+        # print time
+        # print (alarm_info["time"] + " " + alarm_info["date"]) == "05:07 2016-11-17"
+        print alarm_time.hour
         return redirect('/')
         # set_alarm( alarm_info )
     else:
